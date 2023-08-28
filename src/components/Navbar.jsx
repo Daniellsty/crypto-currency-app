9a63e012cb79e22b301df181da9a6a5ced4dad25
danyal <danyallsty04@gmail.com>
2023-08-28 07:14:12 -0700

    toggle navbar and resizing added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from  '../images/cryptocurrency.png'
 import { NavLink } from 'react-router-dom';
 const Navbar = () => {
+
+  const [activeMenu,setActiveMenu] = useState(false)
+  const [screenSize,setScreenSize] = useState(null)
+
+  useEffect(() => {
+
+    const handleResize=()=>setScreenSize(window.innerWidth)
+    
+    
+    window.addEventListener('resize',handleResize)
+    
+    handleResize()
+
+    return ()=> window.removeEventListener('resize',handleResize)
+
+    
+    
+  }, []);
+
+  useEffect(() => {
+
+    if(screenSize > 768){
+
+      setActiveMenu(true)
+    }else{
+      setActiveMenu(false)
+    }
+
+
+    
+    
+  }, [screenSize]);
+
+
+
+
+
   return (
     <div>
 <div className="nav-container">
@@ -12,23 +49,25 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
             <NavLink to="/">Cryptoverse</NavLink>
             </Typography.Title>
-        <Button className="menu-control-container" ><MenuOutlined /></Button>
+        <Button onClick={()=> setActiveMenu( prev=> !prev )} className="menu-control-container" ><MenuOutlined /></Button>
       </div>
      
-      <Menu  theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <NavLink to="/">Home</NavLink>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <NavLink to="/cryptocurrencies">Cryptocurrencies</NavLink>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <NavLink to="/exchanges">Exchanges</NavLink>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <NavLink to="/news">News</NavLink>
-        </Menu.Item>
-      </Menu>
+    {activeMenu &&
+    <Menu  theme="dark">
+    <Menu.Item icon={<HomeOutlined />}>
+      <NavLink to="/">Home</NavLink>
+    </Menu.Item>
+    <Menu.Item icon={<FundOutlined />}>
+      <NavLink to="/cryptocurrencies">Cryptocurrencies</NavLink>
+    </Menu.Item>
+    <Menu.Item icon={<MoneyCollectOutlined />}>
+      <NavLink to="/exchanges">Exchanges</NavLink>
+    </Menu.Item>
+    <Menu.Item icon={<BulbOutlined />}>
+      <NavLink to="/news">News</NavLink>
+    </Menu.Item>
+  </Menu>  
+  }
 
     </div>
     </div>
