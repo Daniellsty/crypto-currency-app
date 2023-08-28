11e8a6af1126b2d211bb8a542abad590f0b73453
danyal <danyallsty04@gmail.com>
2023-08-28 06:54:13 -0700

    fetch data for crypto details added

---
+++ src/Services/data.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+const cryptoApiHeaders  =  {
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'dfebd02829msh59e5d8c3b3a74b3p1b7c52jsn3e2cf1190485'
+  }
+
+  const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+
+  const createRequest =(url)=> ({url,headers:cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCrypto:builder.query({
+        query:(count)=> createRequest(`/coins?limit=${count}`)
+      }),
+      getCryptoDetail:builder.query({
+        query:(coinId)=> createRequest(`/coin/${coinId}`),
+      }),
+      getCryptoHistory:builder.query({
+        query:({coinId,timeperiod})=> createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+      }),
+      }),
+    })
+
+export const {useGetCryptoQuery, useGetCryptoDetailQuery,useGetCryptoHistoryQuery} = cryptoApi;
